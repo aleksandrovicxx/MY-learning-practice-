4ec1c4177b0d8a6475d17eb1ca225460be286c5a
aleksandrovicxx <aleksandrovicmilos99@gmail.com>
2024-04-14 20:19:51 +0200

    comm

---
+++ 22_TO_DO_LISTA/Verzija3/script.js
@@ -0,0 +1,33 @@
+//DOM
+let liZadaci = document.querySelectorAll(`li`);
+let btnDodaj = document.getElementById("submit");
+let inputZadatka = document.querySelector("#task");
+let ulZadaci = document.querySelector("ul")
+let form = document.getElementById("form")
+
+ulZadaci.addEventListener("click", e => {
+    if(e.target.tagName == "LI"){
+        // e.target.classList.toggle("precrtaj")
+        e.target.remove();
+    }
+});
+
+inputZadatka.addEventListener("keyup", e => {
+    // e.preventDefault();
+    if(e.keyCode == 13){
+        let tekstZadatka = inputZadatka.value;  
+    if(inputZadatka.value.trim() != ""){
+        let liNoviZadatak = document.createElement("li"); // Kreiram novi li
+        liNoviZadatak.innerHTML = tekstZadatka; // Dodajem tekst u <li>
+
+        let radioDodaj = document.querySelector("input[name='add']:checked") 
+        if(radioDodaj.value == "beggin") {
+            ulZadaci.prepend(liNoviZadatak);
+        } else {
+            ulZadaci.appendChild(liNoviZadatak)
+        }
+    }
+    inputZadatka.value = ""; 
+    }
+});
+
